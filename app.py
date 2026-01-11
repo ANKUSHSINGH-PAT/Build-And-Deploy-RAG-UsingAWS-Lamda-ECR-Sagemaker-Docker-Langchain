00e29c627b3be772c7d87300536e6d5193da4837
Ankush <singhankush06@gmail.com>
2026-01-11 20:15:21 +0530

    app.py created

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
-import os
-import boto3
-import sys
 import json
-print("Imported modules successfully.")
+import boto3
 
-prompt="""You are a smart assistant. Please let me know what is machine learning?
+bedrock = boto3.client("bedrock-runtime", region_name="us-east-1")
+
+prompt = """
+<|begin_of_text|>
+<|system|>
+You are a helpful AI assistant. Give clear and concise explanations.
+<|end_of_system|>
+
+<|user|>
+When RCB will win the title?
+<|end_of_user|>
+
+<|assistant|>
 """
 
-payload={
-
+payload = {
+    "prompt": prompt,
+    "max_gen_len": 512,
+    "temperature": 0.7,
+    "top_p": 0.9
 }
-body=json.dumps(payload)
-model_id='meta.llama3-3-70b-instruct-v1:0'
 
-bedrock=boto3.client(service_name='badrock-runtime')
-response=bedrock.invoke_model(
-    body=body,
-    modelId=model_id,
-    accept='application/json',
-    contentType='application/json'
-
+response = bedrock.invoke_model(
+    modelId="meta.llama3-70b-instruct-v1:0",
+    body=json.dumps(payload),
+    contentType="application/json",
+    accept="application/json"
 )
-json_response=json.loads(response['body'].read().decode())
-response_text=json_response['generatedText']
-print(response_text)
+
+result = json.loads(response["body"].read())
+print(result["generation"])
